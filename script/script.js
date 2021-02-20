1ea6a8b916868b8a4e11f51858d91d0b3c16bf36
Breno César <44047399+EIleven@users.noreply.github.com>
2021-02-20 16:16:13 -0300

    att-aula10

---
+++ script/script.js
@@ -0,0 +1,28 @@
+var a = window.document.getElementById('area')
+a.addEventListener('click', clicar)
+a.addEventListener('mouseenter', entrar)
+a.addEventListener('mouseout', sair)
+
+function clicar() {
+    a.innerText = 'Clicou!'
+    a.style.background = 'green'
+}
+function entrar() {
+    a.innerText = 'Entrou!'
+    a.style.background = 'crimson'
+}
+function sair() {
+    a.innerText = 'Saiu!'
+    a.style.background = 'red'
+}
+
+function somar() {
+    var tn1 = window.document.getElementById('txtn1')
+    var tn2 = window.document.querySelector('input#txtn2')
+    var res = window.document.getElementById('res')
+    var n1 = Number(tn1.value)
+    var n2 = Number(tn2.value)
+    var s = n1 + n2
+    res.innerHTML = `A soma entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
+    
+}
