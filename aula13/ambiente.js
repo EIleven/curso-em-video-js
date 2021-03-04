69e674cfb413d7a5ce080d5e6b194c6533fc8aee
Breno César <44047399+EIleven@users.noreply.github.com>
2021-03-03 21:02:51 -0300

    att-aula13-14

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1 // repetições com teste lógico no final
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+/* repetições com teste lógico no início
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++
+}
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
