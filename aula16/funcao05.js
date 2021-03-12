64c677abca148bd9212dd4f54173bf8f1fe6dd59
Breno César <44047399+EIleven@users.noreply.github.com>
2021-03-12 20:56:00 -0300

    add-aula16

---
+++ aula16/funcao05.js
@@ -0,0 +1,10 @@
+// FATORIAL COM FUNÇÃO RECURSIVA
+function fatorial(n) {
+    if (n == 1) {
+        return 1
+    } else {
+        return n * fatorial(n - 1)
+    }
+}
+
+console.log(fatorial(5))
---
+++ aula16/funcoes02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) { // COM PARÂMETROS PRÉ-DEFINIDOS
+    return n1 + n2
+}
+
+console.log(soma(4, 7))
